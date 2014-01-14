4c78c97d1c902f42d0da45d1f9030f795316b80f
Saysa <saysabou@gmail.com>
2014-01-14 16:12:08 +0100

    create module : router

--- module/Annotate/config/module.config.php
+++ module/Annotate/config/module.config.php
@@ -4,4 +4,22 @@
  * Array contenant l'ensemble des services que le module doit lancer dès le départ
  */
 
-return array();
+return array(
+	// Service
+	'router' => array(
+		// Array contient l'ensemble des chemins
+		'routes' => array(
+			// Nom d'un chemin
+			'myIndex' => array(
+				'type' => 'Zend\Mvc\Router\Http\Literal',
+				'options' => array(
+					'route' => '/',
+					'defaults' => array(
+						'controller' => 'IndexController',
+						'action'	 => 'index',
+					),
+				), 
+			),
+		),
+	),
+);
